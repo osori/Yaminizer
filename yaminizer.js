42db90c8934d3b4effcdfea78ec893270fafe03e
Ilkyu Ju <me@ilkyu.kr>
2017-11-05 14:09:05 +0900

    Add reverse function of yaminize

--- yaminizer.js
+++ yaminizer.js
@@ -27,36 +27,55 @@ var yaminjeongeum = { 'naive rules':
         '근': 'ㄹ',
         '日': '티', } };
 
-var hunmin = Object.keys(yaminjeongeum["naive rules"]);
-var re = new RegExp(hunmin.join('|'), 'g');
+var translate_mode = 0; // 0: 야민정음으로 번역
+                        // 1: 일상어로 되돌리기
 
+// 야민정음으로 바뀔 수 있는 글자/단어들의 리스트를 토대로
+// regex 를 생성
+var hunmin = Object.keys(yaminjeongeum["naive rules"]);
+var re_yaminize = new RegExp(hunmin.join('|'), 'g');
+
+// 일상어 -> 야민정음
 function yaminize(str){
-	return str.replace(re, function(match){
+	return str.replace(re_yaminize, function(match){
 		return yaminjeongeum["naive rules"][match];
 	});
 }
 
+var yamin = Object.values(yaminjeongeum["naive rules"]);
+var re_hunminize = new RegExp(yamin.join('|'), 'g');
+
 // reverse function of yaminize
 function hunminize(str){
-
+	return str.replace(re_hunminize, function(match){
+		return hunmin[yamin.indexOf(match)];
+	});
 }
+
+$('#yaminize').click(function() {
+   if($('#yaminize').is(':checked')) { 
+        translate_mode = 0; 
+        swapIOText();
+    }
+});
+
+$('#hunminize').click(function() {
+   if($('#hunminize').is(':checked')) { 
+       translate_mode = 1; 
+       swapIOText();
+    }
+});
 
 $("#input_text").keyup(function(){
 	var input_text = $("#input_text").val();
-	var result = yaminize(input_text)
+    var result = translate_mode == 0 ? yaminize(input_text) : hunminize(input_text);
 	$("#result").val(result);
 	$('#result').trigger('autoresize');
 })
 
-  $('#input_text').autocomplete({
-    data: {
-      "Apple": null,
-      "Microsoft": null,
-      "Google": 'https://placehold.it/250x250'
-    },
-    limit: 20, // The max amount of results that can be shown at once. Default: Infinity.
-    onAutocomplete: function(val) {
-      // Callback function when value is autcompleted.
-    },
-    minLength: 1, // The minimum length of the input for the autocomplete to start. Default: 1.
-  });
+function swapIOText(){
+    var temp = $("#input_text").val();
+    console.log(temp);
+    $("#input_text").val($("#result").val());
+    $("#result").val(temp);
+}
